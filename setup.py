32c1f7118cced5368b30862daf4f14efbf99b324
Ivan Nazarov <ivannz@yandex.ru>
2021-04-10 23:18:11 +0300

    a setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='RLPlay',
+    description="""Some prototypes for RL and toy experiments.""",
+    version='0.1',
+    license='MIT',
+    packages=[
+        'rlplay'
+    ]
+)
