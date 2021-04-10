cda8475f80c9b7209435848691e40f99e233bdf1
Ivan Nazarov <ivannz@yandex.ru>
2021-04-10 23:39:17 +0300

    linear interp between points

--- rlplay/utils.py
+++ rlplay/utils.py
@@ -16,3 +16,8 @@ class ImageToTensor(ObservationWrapper):
     def observation(self, observation):
         observation = observation.transpose(2, 0, 1)
         return np.divide(observation, 255, dtype=np.float32)
+
+
+def linear(t, t0=0, t1=100, v0=1., v1=0.):
+    tau = min(1., max(0., (t1 - t) / (t1 - t0)))
+    return v0 * tau + v1 * (1 - tau)
