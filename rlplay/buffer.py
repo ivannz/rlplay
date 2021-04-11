00a7ef2e5d8b16b4dc9a1a7e9c1552497455e1f4
ivannz <ivannz@yandex.ru>
2021-04-11 16:03:44 +0300

    written short docs for simplebuffer

--- rlplay/buffer.py
+++ rlplay/buffer.py
@@ -85,30 +85,36 @@ def to_device(batch, *, device):
     elif isinstance(batch, torch.Tensor):
         return batch.to(device)
 
-    # only torch.Tensors can be moved, everything else is kept as is
+    # only `torch.Tensor` can be moved, everything else is kept as is
     return batch
 
 
 class SimpleBuffer:
-    """A simple buffer with arbitary schema which mimics a dataloader."""
+    """A simple ring buffer with no schema which mimics a dataloader."""
     def __init__(self, n_capacity, n_batch_size=32, *, random=None):
         self.n_batch_size = n_batch_size
         self.random_ = random or Random()
         self.buffer = deque(maxlen=n_capacity)
 
     def commit(self, **kwdata):
+        """Put key-value data into the buffer, evicting the oldest record if full."""
         self.buffer.append(kwdata)
 
     def __len__(self):
+        """The used capacity of the buffer."""
         return len(self.buffer)
 
     def __getitem__(self, index):
+        """Get the item at a particular index in the buffer."""
         return self.buffer[index]
 
     def __iter__(self):
+        """Iterate over random batches of key-value data from the buffer."""
         permutation = self.random_.sample(range(len(self)), len(self))
         for ix in range(0, len(self), self.n_batch_size):
             batch = permutation[ix:ix+self.n_batch_size]
+
+            # use torch's versatile default collate function
             yield torch_collate([
-                {**self[j], '_index': j} for j in batch  # add reference
+                {**self[j], '_index': j} for j in batch  # add back reference
             ])
