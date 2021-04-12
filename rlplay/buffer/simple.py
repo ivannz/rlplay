246b7886e2cbb6e53902d64b55c92ca2949871a5
ivannz <ivannz@yandex.ru>
2021-04-12 19:05:29 +0300

    comments on performance

--- rlplay/buffer/simple.py
+++ rlplay/buffer/simple.py
@@ -7,7 +7,7 @@ from torch.utils.data.dataloader import default_collate as torch_collate
 
 
 class SimpleBuffer:
-    """A simple ring buffer with no schema which mimics a dataloader."""
+    """A simple slow ring buffer with no schema which mimics a dataloader."""
     def __init__(self, n_capacity, n_batch_size=32, *, random=None):
         self.n_batch_size = n_batch_size
         self.random_ = random or Random()
@@ -19,6 +19,7 @@ class SimpleBuffer:
 
     def __len__(self):
         """The used capacity of the buffer."""
+        # XXX together with `__iter__`, this confuses tqdm.
         return len(self.buffer)
 
     def __getitem__(self, index):
@@ -27,11 +28,13 @@ class SimpleBuffer:
 
     def __iter__(self):
         """Iterate over random batches of key-value data from the buffer."""
+        # XXX `Random.sample` is not very fast performing
         permutation = self.random_.sample(range(len(self)), len(self))
         for ix in range(0, len(self), self.n_batch_size):
             batch = permutation[ix:ix+self.n_batch_size]
 
-            # use torch's versatile default collate function
+            # use torch's versatile but slow default collate function
+            # XXX moving it out of the loop does not seem to yield speed ups
             yield torch_collate([
                 {**self[j], '_index': j} for j in batch  # add back reference
             ])
