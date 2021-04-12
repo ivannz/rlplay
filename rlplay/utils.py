81e68ab15277c24b9f3b76568c5e017dd8c634c1
ivannz <ivannz@yandex.ru>
2021-04-12 12:45:33 +0300

    simple eps-greedy

--- rlplay/utils.py
+++ rlplay/utils.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 
 from gym import ObservationWrapper
@@ -21,3 +22,14 @@ class ImageToTensor(ObservationWrapper):
 def linear(t, t0=0, t1=100, v0=1., v1=0.):
     tau = min(1., max(0., (t1 - t) / (t1 - t0)))
     return v0 * tau + v1 * (1 - tau)
+
+
+@torch.no_grad()
+def greedy(q_value, *, epsilon=0.5):
+    """epsilon-greedy strategy."""
+    q_action = q_value.max(dim=-1).indices
+
+    random = torch.randint(q_value.shape[-1], size=q_action.shape,
+                           device=q_value.device)
+    is_random = torch.rand_like(q_value[..., 0]).lt(epsilon)
+    return torch.where(is_random, random, q_action)
