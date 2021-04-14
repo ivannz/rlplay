0ddc9e0ad1d74e90426ea0b46ca96e82b64c9e5d
ivannz <ivannz@yandex.ru>
2021-04-14 23:51:59 +0300

    dummy image viewer and export conditionally on pyglet

--- rlplay/utils/plotting/__init__.py
+++ rlplay/utils/plotting/__init__.py
@@ -1,6 +1,13 @@
 from .grid import make_grid
 
-from .imshow import ImageViewer
-from .conv2d import Conv2DViewer
-
 from .dummy import Conv2DViewer as DummyConv2DViewer
+from .dummy import ImageViewer as DummyImageViewer
+
+try:
+    # pyglet fails to acquire a display and raises
+    from .imshow import ImageViewer
+    from .conv2d import Conv2DViewer
+
+# pyglet.canvas.xlib.NoSuchDisplayException: Cannot connect to "None"
+except Exception:
+    Conv2DViewer, ImageViewer = DummyConv2DViewer, DummyImageViewer
--- rlplay/utils/plotting/dummy.py
+++ rlplay/utils/plotting/dummy.py
@@ -1,3 +1,30 @@
+class ImageViewer:
+    def __init__(self, *args, **kwargs):
+        self._was_closed = False
+
+    def close(self):
+        self._was_closed = True
+
+    def on_close(self):
+        self.close()
+
+    @property
+    def isopen(self):
+        return not self._was_closed
+
+    def __getattr__(self, name):
+        if not name.startswith('on_'):
+            raise AttributeError(name)
+
+        return lambda *a, **k: None
+
+    def imshow(self, *args, **kwargs):
+        pass
+
+    def render(self):
+        pass
+
+
 class Conv2DViewer:
     def __enter__(self):
         return self
