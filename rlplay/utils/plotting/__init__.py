bf431d7c7a2c17c28c0dcb5540a0c7d725e8e543
ivannz <ivannz@yandex.ru>
2021-04-14 17:54:11 +0300

    dummy viewer

--- rlplay/utils/plotting/__init__.py
+++ rlplay/utils/plotting/__init__.py
@@ -1,4 +1,4 @@
 from .grid import make_grid
 
 from .imshow import ImageViewer
-from .conv2d import Conv2DViewer
+from .conv2d import Conv2DViewer, DummyConv2DViewer
--- rlplay/utils/plotting/conv2d.py
+++ rlplay/utils/plotting/conv2d.py
@@ -95,5 +95,13 @@ class Conv2DViewer(BaseModuleHook):
         self.draw()
 
 
+class DummyConv2DViewer:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+
 if __name__ == '__main__':
     pass
