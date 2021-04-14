6a5f31fb9d22b17843b37dfbba202da40d90b48a
ivannz <ivannz@yandex.ru>
2021-04-14 13:54:04 +0300

    expose module hook

--- rlplay/utils/__init__.py
+++ rlplay/utils/__init__.py
@@ -3,6 +3,7 @@ from .common import linear, greedy
 from .wrappers import ToTensor, ChannelFirst, AtariObservation
 from .wrappers import ObservationQueue, FrameSkip
 
+from .module import BaseModuleHook
 from .schema import ensure, to_device
 
 # from .imshow import ImageViewer  # not exported due to heavy dependencies
