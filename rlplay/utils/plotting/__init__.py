cb1040e734f6de963260b7dd602b4f7aebff1602
ivannz <ivannz@yandex.ru>
2021-04-14 23:42:06 +0300

    separate actual from dummy for importing in headless py environments

--- rlplay/utils/plotting/__init__.py
+++ rlplay/utils/plotting/__init__.py
@@ -1,4 +1,6 @@
 from .grid import make_grid
 
 from .imshow import ImageViewer
-from .conv2d import Conv2DViewer, DummyConv2DViewer
+from .conv2d import Conv2DViewer
+
+from .dummy import Conv2DViewer as DummyConv2DViewer
---
+++ rlplay/utils/plotting/dummy.py
@@ -0,0 +1,6 @@
+class Conv2DViewer:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
