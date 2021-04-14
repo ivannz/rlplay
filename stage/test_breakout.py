c7ee287a394993b5aba26e8c7735e3a7ce7c82c7
ivannz <ivannz@yandex.ru>
2021-04-14 18:04:08 +0300

    breakout test run

---
+++ stage/test_breakout.py
@@ -0,0 +1,69 @@
+import os
+import gym
+import time
+import torch
+
+from rlplay.utils import ToTensor
+from rlplay.utils import AtariObservation, ObservationQueue, FrameSkip
+from rlplay.zoo.toy import BreakoutQNet
+
+from rlplay.utils import greedy
+
+from rlplay.utils.plotting import Conv2DViewer
+
+
+path_ckpt = os.path.join(os.path.abspath('./runs'), 'checkpoints')
+device = torch.device('cpu')
+
+# an instance of atari Breakout-v4
+env = gym.make('BreakoutNoFrameskip-v4')
+# env = TerminateOnLostLive(env)  # messes up the randomness of ALE
+env = AtariObservation(env, shape=(84, 84))
+env = ToTensor(env)
+env = FrameSkip(env, n_frames=4, kind='max')
+env = ObservationQueue(env, n_size=4)
+
+q_net = BreakoutQNet(env.action_space.n).to(device)
+
+checkpoint = os.path.join(path_ckpt, 'latest.pt')
+if os.path.isfile(checkpoint):
+    q_net.load_state_dict(torch.load(checkpoint)['q_net'])
+
+state_ = torch.empty(1, *env.observation_space.shape,
+                     dtype=torch.float32, device=device)
+
+
+# infinite post training rollout
+@torch.no_grad()
+def rollout(module, viewer=None):
+    env.seed(42)
+    obs = env.reset()
+    done, totrew, n_step = False, 0., 0
+
+    module.eval()
+    while not done:
+        if not env.render(mode='human'):
+            return False
+        time.sleep(0.01)
+
+        state_[0].copy_(torch.from_numpy(obs))
+        with viewer:
+            q_value = module(state_).squeeze(0)
+
+        # print(q_value.cpu().numpy())
+        action = int(greedy(q_value, epsilon=0.1))
+        obs, reward, done, info = env.step(action)
+
+        totrew += reward
+        n_step += 1
+
+    if totrew > 0:
+        print(f'{n_step} :: {totrew}')
+
+    return True
+
+
+viewer = Conv2DViewer(q_net, activation=torch.relu, pixel=(5, 5))
+with env:
+    while rollout(q_net, viewer):
+        pass
