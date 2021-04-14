43984a7ab0a2ef883673939c78dccfc4a9f7dea8
ivannz <ivannz@yandex.ru>
2021-04-15 01:20:18 +0300

    make dummy conv2d-viewer have similar api to the non-dummy version

--- rlplay/utils/plotting/dummy.py
+++ rlplay/utils/plotting/dummy.py
@@ -30,4 +30,20 @@ class Conv2DViewer:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        pass
+        self.draw()
+
+    def exit(self):
+        self.close()
+
+    def __iter__(self):
+        return []
+
+    @property
+    def isopen(self):
+        return not self._was_closed
+
+    def close(self):
+        self._was_closed = True
+
+    def draw(self):
+        self._was_closed = False
