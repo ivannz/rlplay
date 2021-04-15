34553b5c9c62cb5417d53654f0600f898c5309b9
ivannz <ivannz@yandex.ru>
2021-04-15 18:19:01 +0300

    pixel attn layer from Zhao et al. (2021)

--- rlplay/zoo/base.py
+++ rlplay/zoo/base.py
@@ -1,7 +1,11 @@
 import torch
+from torch.nn import Conv2d, Module
 
 
 class Duelling(torch.nn.Module):
+    """Duelling q-net head from
+        [Wang et al. (2016)](http://arxiv.org/abs/1511.06581)
+    """
     def __init__(self, *, value, advantage):
         super().__init__()
         self.value, self.advantage = value, advantage
@@ -9,3 +13,20 @@ class Duelling(torch.nn.Module):
     def forward(self, input):
         a, v = self.advantage(input), self.value(input)
         return a + (v - a.mean(dim=-1, keepdim=True))
+
+
+class PixelAttnConv(Module):
+    """Pixel attention from
+        [Zhao et al. (2021)](https://arxiv.org/abs/2010.01073) fig. 3
+    """
+    def __init__(self, n_channels, kernel=3):
+        super().__init__()
+        self.k1 = Conv2d(n_channels, n_channels, 1, stride=1, bias=True)
+        self.k2 = Conv2d(n_channels, n_channels, kernel, stride=1,
+                         padding=(kernel - 1)//2, bias=True)
+        self.k3 = Conv2d(n_channels, n_channels, kernel, stride=1,
+                         padding=(kernel - 1)//2, bias=True)
+
+    def forward(self, input):
+        attn = self.k1(input).sigmoid()
+        return self.k3(attn.mul(self.k2(input)))
