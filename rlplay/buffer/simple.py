334d77e6ff59bfcfd4ecc7acd769f638c31ba72f
ivannz <ivannz@yandex.ru>
2021-04-15 21:59:42 +0300

    use torch-s generator, change buffer API

--- rlplay/buffer/simple.py
+++ rlplay/buffer/simple.py
@@ -1,25 +1,24 @@
 import torch
 
-from random import Random
-
 from torch.utils.data.dataloader import default_collate as torch_collate
 
 
 class SimpleBuffer:
     """A simple slow ring buffer with no schema which mimics a dataloader."""
-    def __init__(self, n_capacity, n_batch_size=32, *, random=None):
-        self.random_ = random or Random()
-        self.n_capacity, self.n_batch_size = n_capacity, n_batch_size
-        self.buffer, self.position = [], 0
+    def __init__(self, capacity, *, generator=None):
+        self.generator_ = generator or torch.default_generator
+        assert isinstance(self.generator_, torch.Generator)
 
-    def commit(self, **kwdata):
+        self.buffer, self.capacity, self.position = [], capacity, 0
+
+    def commit(self, _index=None, **kwdata):
         """Put key-value data into the buffer, evicting the oldest record if full."""
-        if len(self.buffer) < self.n_capacity:
+        if len(self.buffer) < self.capacity:
             self.buffer.append(kwdata)  # grow buffer
 
         else:
             self.buffer[self.position] = kwdata
-        self.position = (1 + self.position) % self.n_capacity
+        self.position = (1 + self.position) % self.capacity
 
     def __len__(self):
         """The used capacity of the buffer."""
@@ -30,19 +29,31 @@ class SimpleBuffer:
         """Get the item at a particular index in the buffer."""
         return self.buffer[index]
 
-    def __setitem__(self, index, value):
+    def __setitem__(self, index, meta):
+        """Associate metadata with a key-value data at the given index."""
         # item attribute assignment is ingored by simple buffers
         pass
 
-    def __iter__(self):
-        """Iterate over random batches of key-value data from the buffer."""
-        # XXX `Random.sample` is not very fast performing
-        permutation = self.random_.sample(range(len(self)), len(self))
-        for ix in range(0, len(self), self.n_batch_size):
-            batch = permutation[ix:ix+self.n_batch_size]
+    def draw(self, batch_size):
+        """Draw a random batch from the buffer WITH replacement."""
+        # `.randperm` is slow for very large buffer, especially if only
+        #  one batch is requested.
+        batch = torch.randint(len(self), size=batch_size,
+                              generator=self.generator_)
+        return self.collate(batch.tolist())  # for-looping over a list fast
 
-            # use torch's versatile but slow default collate function
-            # XXX moving it out of the loop does not seem to yield speed ups
-            yield torch_collate([
-                {**self[j], '_index': j} for j in batch  # add back reference
-            ])
+    def sample(self, batch_size):
+        """Iterate over the buffer in batches drawn WITHOUT replacement."""
+        n_batches = (len(self) + batch_size - 1) // batch_size
+
+        permutation = torch.randperm(len(self), generator=self.generator_)
+        # permutation[j::n_batches] is `statistically` the same permutation
+        for j in range(n_batches):
+            # XXX `torch_collate` on the whole buffer gives no speed up
+            yield self.collate(permutation[j::n_batches].tolist())
+
+    def collate(self, indices):
+        # use torch's versatile, but slow-ish default collate function
+        return torch_collate([
+            {**self.buffer[j], '_index': j} for j in indices  # back reference
+        ])
