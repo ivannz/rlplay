9c5a887334a11ba00a321227dfd256ea57877693
ivannz <ivannz@yandex.ru>
2021-04-15 22:59:09 +0300

    faster prioritized buffer

--- rlplay/buffer/priority.py
+++ rlplay/buffer/priority.py
@@ -1,9 +1,5 @@
 import torch
 
-from array import array
-from operator import add
-from random import Random
-
 from torch.utils.data.dataloader import default_collate as torch_collate
 
 
@@ -81,63 +77,76 @@ class PriorityBuffer:
     which is the ratio of the item's probability $p_j$ to the probability
     of the rarest item.
     """
-    def __init__(self, n_capacity, n_batch_size=32, *, alpha=1., random=None):
-        self.random_ = random or Random()
-        self.n_capacity, self.n_batch_size = n_capacity, n_batch_size
-        self.buffer, self.position = [], 0
+    def __init__(self, capacity, *, generator=None, alpha=1.):
+        self.generator_ = generator or torch.default_generator
+        assert isinstance(self.generator_, torch.Generator)
 
-        # get the nearest greater power of two for `n_capacity`
-        n_capacity = 1 << (n_capacity - 1).bit_length()
-        self._sum = array('f', [0.]) * (2 * n_capacity)  # fast sums
-        self._min = array('f', [float('+inf')]) * (2 * n_capacity)  # fast mins
+        self.buffer, self.capacity, self.position = [], capacity, 0
 
         self.default, self.alpha = 1., alpha
+        self._prio = torch.full((2 * capacity,), self.default, dtype=float)
 
-    def commit(self, **kwdata):
-        """Put key-value data into the buffer, evicting the oldest record if full."""
-        _update(add, self._sum, self.position, self.default ** self.alpha)
-        _update(min, self._min, self.position, self.default ** self.alpha)
-        if len(self.buffer) < self.n_capacity:
-            self.buffer.append(kwdata)  # grow buffer
+    @property
+    def priority(self):
+        offset = len(self._prio) // 2
+        return self._prio[offset:offset + len(self)]
 
-        else:
-            self.buffer[self.position] = kwdata
-        self.position = (1 + self.position) % self.n_capacity
+    @property
+    def min(self):
+        return self._prio[1]
 
     def __len__(self):
         """The used capacity of the buffer."""
-        # XXX together with `__iter__`, this confuses tqdm.
         return len(self.buffer)
+
+    def __iter__(self):
+        """iterate over the content."""
+        return iter(self.buffer)
 
     def __getitem__(self, index):
         """Get the item at a particular index in the buffer."""
         return self.buffer[index]
 
+    def commit(self, _index=None, _weight=None, **kwdata):
+        """Put key-value data into the buffer, evicting the oldest record if full."""
+        _update(min, self._prio, self.position, self.default ** self.alpha)
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(kwdata)  # grow buffer
+
+        else:
+            self.buffer[self.position] = kwdata
+        self.position = (1 + self.position) % self.capacity
+
     def __setitem__(self, index, value):
         """Set the priority of an index in the buffer."""
+        assert value > 0.
         self.default = max(self.default, value)
-        _update(add, self._sum, index, value ** self.alpha)
-        _update(min, self._min, index, value ** self.alpha)
+        _update(min, self._prio, index, value ** self.alpha)
 
-    def __iter__(self):
-        return self
+    def draw(self, batch_size, replacement=True):
+        """Draw a random batch from the buffer WITH replacement."""
+        batch = self.priority.multinomial(batch_size, replacement=replacement,
+                                          generator=self.generator_)
+        return self.collate(batch.tolist())
 
-    def __next__(self):
-        """Draw a prioritized batch of key-value data from the buffer."""
-        indices = [_prefix(self._sum, self.random_.random() * self._sum[1])
-                   for _ in range(self.n_batch_size)]
+    def sample(self, batch_size, replacement=False):
+        """Iterate over the buffer in batches respecting priority."""
+        sample = self.priority.multinomial(batch_size, replacement=replacement,
+                                           generator=self.generator_)
+        for j in range(0, len(self), batch_size):
+            yield self.collate(sample[j:j + batch_size].tolist())
 
+    def collate(self, indices):
         # Importance weights are $\omega_j = \frac{\pi_j}{\beta_j}$, for
-        #  target distributinon `\pi` and sampling distributinon `\beta`
+        #  target distribution `\pi` and sampling distribution `\beta`.
         # We assume uniform $\pi_j = \frac1N$ and prioritized $
         #    \beta_j = \frac{p_j^\alpha}{\sum_k p_k^\alpha}
-        # $, and normalize by the maximal weight `\max_j \omega_j`. After
+        # $, and return weights normalized by `\max_j \omega_j`. After
         # simplification this yields the formula below.
+        buf, batch = self.buffer, []
+        for j in indices:
+            batch.append({
+                **buf[j], '_weight': self.min / self.priority[j], '_index': j
+            })
 
-        # use torch's versatile but slow default collate function
-        # XXX moving it out of the loop does not seem to yield speed ups
-        return torch_collate([
-            {**self[j], '_index': j,
-             '_weight': self._min[1] / _value(self._sum, j)
-             } for j in indices
-        ])
+        return torch_collate(batch)
