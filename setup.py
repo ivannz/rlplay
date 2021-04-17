b4c85dee3a6fe198593caee690f877fbfe54f8e4
ivannz <ivannz@yandex.ru>
2021-04-17 19:09:36 +0300

    a more diverse zoo

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import setup
 setup(
     name='RLPlay',
     description="""Some prototypes for RL and toy experiments.""",
-    version='0.1',
+    version='0.2',
     license='MIT',
     packages=[
         'rlplay',
         'rlplay.zoo',  # models and implementations
+        'rlplay.zoo.env',  # environments for testing and experimenting
+        'rlplay.zoo.models',  # models, policies and q-nets
         'rlplay.buffer',  # replay, rollout and other buffers
         'rlplay.algo',  # losses, pieces of algorithms
         'rlplay.utils',  # wrappers, plotting, and other tools
