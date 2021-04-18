20f22fd9ce841a045b165f8cb5f8c6082512c70f
ivannz <ivannz@yandex.ru>
2021-04-18 12:33:10 +0300

    dedicated get-display func, docs for all viewer-s methods, extended description for .imshow

--- rlplay/utils/plotting/imshow.py
+++ rlplay/utils/plotting/imshow.py
@@ -13,12 +13,37 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 
+def get_display(display):
+    if display is None:
+        return pyglet.canvas.get_display()
+
+    elif isinstance(display, pyglet.canvas.Display):
+        return display
+
+    elif isinstance(display, str):
+        return pyglet.canvas.Display(display)
+
+    raise RuntimeError('`display` must be a Display object, an str, like ":0",'
+                       f' or None for a default display. Got `{display}`.')
+
+
 class ImageViewer(Window):
-    """My own version of the image viewer.
+    """A window displaying an image or matplotlib figure.
+
+    Parameters
+    ----------
+    caption : str, or None
+        The caption of the viewer window. Defaults to the current script
+        filename if None.
+
+    display : str, or None
+        Uses the primary display if `None`, otherwise puts the viewer on the
+        specified display. `pyglet` only supports multiple displays on Linux.
     """
-    def __init__(self, caption=None):
+
+    def __init__(self, caption=None, *, display=None):
         super().__init__(caption=caption, resizable=True, vsync=False,
-                         display=pyglet.canvas.get_display())
+                         display=get_display(display))
 
         # randomly displace the window
         pos_y = randint(0, max(self.screen.height - self.height, 0))
@@ -28,19 +53,23 @@ class ImageViewer(Window):
         # XXX maybe use FPSDisplay from `pyglet.window`
 
     def close(self):
+        """Close the viewer window."""
         # avoid `Python is likely shutting down` ImportError raised by cocoa
         if not sys.is_finalizing():
             super().close()
 
     def on_close(self):
+        """User pressed the `X` button on the window to close it."""
         super().on_close()
         self.close()  # super().__del__() also invokes this method
 
     @property
     def isopen(self):
+        """Flag indicating if a viewer window is open."""
         return not self._was_closed  # self.close() sets this flag
 
     def on_key_press(self, symbol, modifiers):
+        """User pressed `SPACE` to fit viewer to the current image's dims."""
         if hasattr(self, 'texture') and symbol == key.SPACE:
             self.set_size(self.texture.width, self.texture.height)
             return
@@ -48,7 +77,7 @@ class ImageViewer(Window):
         super().on_key_press(symbol, modifiers)
 
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
-        """Move the window live on mouse drag."""
+        """Move the window around when the user drags it with a mouse."""
         if buttons & mouse.LEFT:
             x, y = self.get_location()
             self.set_location(x + dx, y - dy)
@@ -57,6 +86,7 @@ class ImageViewer(Window):
         super().on_mouse_drag(x, y, dx, dy, buttons, modifiers)
 
     def on_draw(self, *, flip=False):
+        """Redraw the current image."""
         self.switch_to()
         self.clear()
 
@@ -71,24 +101,42 @@ class ImageViewer(Window):
         if flip:
             self.flip()
 
-    def imshow(self, data, *, resize=False):
+    def imshow(self, data, *, keepdims=True):
         """Show the monochrome, RGB or RGBA image data, or a matplotlib Figure.
+
+        Parameters
+        ----------
+        data : numpy.ndarray, or matplotlib.figure.Figure
+            The data to display in the viewer.
+            * If `data` is a matplotlib Figure, that has already been composed
+              elsewhere, then it gets rendered into a 3d RGBA array and
+              displayed as color image.
+            * If `data` is a numpy array, then it must either be a 2d or a 3d
+              array for a grayscale or color image, respectively. In the color
+              image case the last dim of the array must have size `3` for an
+              RGB image or `4` for and RGBA image (RGB with an alpha channel).
+
+        keepdims : bool, default=True
+            Whether to preserve the current dimensions of the viewer, or resize
+            it to fit the width and height of the current image.
 
         Details
         -------
         To prevent memory leaks it is advisable to close the figure afterwards,
         using `plt.close(fig)`. However, creating a new figure and then closing
-        it appears to incur too much overhead. Hence, it is recommended to
-        create a figure and axes only onсe, and then clear them after every
-        call to `.imshow`. Lowering `figsize` and `dpi` may also improve fps.
+        it appears to incur excessive overhead. One possible workaround is to
+        create a figure and axes only once, and, when updating it, recreate its
+        contents on the axes before and clear them after any call to `.imshow`.
+        In general matplotlib is not optimized for live redrawing, but lowering
+        `figsize` and `dpi` may improve fps.
         """
 
-        # use the `agg` backend directly to create rgba arrays from figures
+        # use the `agg` backend directly to create RGBA arrays from figures
         # https://matplotlib.org/stable/gallery/user_interfaces/canvasagg.html
         if isinstance(data, Figure):
             # assume all necessary plotting and artistry has been done
-            canvas = FigureCanvasAgg(data)
-            # render into a buffer and convert to numy arrray
+            canvas = FigureCanvasAgg(data)  # XXX does not seem to leak mem
+            # render into a buffer and convert to numpy array
             canvas.draw()
             data = np.array(canvas.buffer_rgba())
 
@@ -120,15 +168,14 @@ class ImageViewer(Window):
             pitch=-width * len(format)).get_texture()
 
         # resize if instructed or on the first call to `.imshow`
-        if resize or not hasattr(self, 'texture'):
+        if not hasattr(self, 'texture') or not keepdims:
             self.set_size(texture.width, texture.height)
         self.texture = texture
 
         self.render()
 
     def render(self):
-        self.switch_to()
-
+        """Respond to the recent window and redraw the current image."""
         # a piece of event loop
         self.dispatch_events()
 
