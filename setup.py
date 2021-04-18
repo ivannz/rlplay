1bb8a1df6f50befe99783233830340a5eabcc735
ivannz <ivannz@yandex.ru>
2021-04-18 16:13:47 +0300

    declare patches and bump minor

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='RLPlay',
     description="""Some prototypes for RL and toy experiments.""",
-    version='0.2',
+    version='0.3',
     license='MIT',
     packages=[
         'rlplay',
@@ -14,11 +14,13 @@ setup(
         'rlplay.algo',  # losses, pieces of algorithms
         'rlplay.utils',  # wrappers, plotting, and other tools
         'rlplay.utils.plotting',  # custom imshow, conv2d visualizer and other
+        'rlplay.utils.integration',  # patches to various other pacakges
     ],
     install_requires=[
         'torch',
         'numpy',
         'gym[atari]',
         'opencv-python',
+        # 'pyglet',  # installed by gym anyway, but we still declare it here
     ]
 )
