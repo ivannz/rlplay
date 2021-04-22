a4bbc626316ae153a6c58e2700065e6b5d500748
ivannz <ivannz@yandex.ru>
2021-04-22 09:13:18 +0300

    the -too model

--- rlplay/zoo/models/breakout.py
+++ rlplay/zoo/models/breakout.py
@@ -75,3 +75,36 @@ class DuellingBreakoutQNet(torch.nn.Module):
                 ),
             ),
         )
+
+
+class BreakoutQNetToo(Module):
+    def __new__(cls, n_outputs, *, batch_norm=True):
+        layers = [
+            ('conv_0', Conv2d(4, 32, 8, 4, bias=not batch_norm)),
+            ('bnorm0', BatchNorm2d(32, affine=True)) if batch_norm else None,
+            ('relu_0', ReLU()),
+            ('tap_0', Identity()),  # nop tap for viewer
+
+            # ('drop_1', Dropout(p=0.5)),
+            ('conv_1', Conv2d(32, 64, 4, 2, bias=not batch_norm)),
+            ('bnorm1', BatchNorm2d(64, affine=True)) if batch_norm else None,
+            ('relu_1', ReLU()),
+            ('tap_1', Identity()),  # nop tap for viewer
+
+            # ('drop_2', Dropout(p=0.2)),
+            ('conv_2', Conv2d(64, 64, 3, 1, bias=not batch_norm)),
+            ('bnorm2', BatchNorm2d(64, affine=True)) if batch_norm else None,
+            ('relu_2', ReLU()),
+            ('tap_2', Identity()),  # nop tap for viewer
+
+            ('flat_3', Flatten(1, -1)),
+            ('drop_3', Dropout(p=0.5)),
+            ('dense3', Linear(64*7*7, 512, bias=True)),
+            ('relu_3', ReLU()),
+
+            ('drop_4', Dropout(p=0.2)),
+            ('dense4', Linear(512, n_outputs, bias=True)),
+        ]
+
+        # filter out `None`s and build a sequential network
+        return Sequential(OrderedDict(list(filter(None, layers))))
