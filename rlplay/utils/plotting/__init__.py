8d03408af003c77470118e7b6b94afedc8a9398f
ivannz <ivannz@yandex.ru>
2021-04-23 15:43:59 +0300

    update dummy objects

--- rlplay/utils/plotting/__init__.py
+++ rlplay/utils/plotting/__init__.py
@@ -2,12 +2,14 @@ from .grid import make_grid
 
 from .dummy import Conv2DViewer as DummyConv2DViewer
 from .dummy import ImageViewer as DummyImageViewer
+from .dummy import MultiViewer as DummyMultiViewer
 
 try:
     # pyglet fails to acquire a display and raises
-    from .imshow import ImageViewer
+    from .imshow import ImageViewer, MultiViewer
     from .conv2d import Conv2DViewer
 
 # pyglet.canvas.xlib.NoSuchDisplayException: Cannot connect to "None"
 except Exception:
     Conv2DViewer, ImageViewer = DummyConv2DViewer, DummyImageViewer
+    MultiViewer = DummyMultiViewer
--- rlplay/utils/plotting/dummy.py
+++ rlplay/utils/plotting/dummy.py
@@ -1,3 +1,10 @@
+
+import numpy as np
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+
+
 class ImageViewer:
     def __init__(self, *args, **kwargs):
         self._was_closed = False
@@ -12,16 +19,73 @@ class ImageViewer:
     def isopen(self):
         return not self._was_closed
 
+    def __bool__(self):
+        return self.isopen
+
     def __getattr__(self, name):
         if not name.startswith('on_'):
             raise AttributeError(name)
 
         return lambda *a, **k: None
 
-    def imshow(self, *args, **kwargs):
-        pass
+    def imshow(self, data, *, keepdims=True):
+        if isinstance(data, Figure):
+            canvas = FigureCanvasAgg(data)
+            canvas.draw()
+            data = np.array(canvas.buffer_rgba())
+
+        self.data = data
+        return self.render()
 
     def render(self):
+        return self.isopen
+
+    def read_buffer(self, format='rgb'):
+        return self.data.copy()
+
+
+class MultiViewer:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @property
+    def isopen(self):
+        return not self._was_closed
+
+    def open(self, *captionless, **captioned):
+        self._was_closed = False
+
+    def on_key_press(self, symbol, modifiers, *, wr_viewer):
+        pass
+
+    def _cycle(self, wr_viewer):
+        pass
+
+    def close(self, *which):
+        self._was_closed = True
+
+    def imshow(self, **content):
+        return self.isopen
+
+    def refresh(self, *which):
+        return self.isopen
+
+    def __bool__(self):
+        return self.isopen
+
+    def __iter__(self):
+        return []
+
+    def __contains__(self, label):
+        return True
+
+    def __getitem__(self, label):
+        return None
+
+    def __setitem__(self, label, content):
+        pass
+
+    def __delitem__(self, label):
         pass
 
 
@@ -33,9 +97,6 @@ class Conv2DViewer:
     def enabled(self):
         return self._enabled
 
-    def register(self, label, module):
-        pass
-
     def toggle(self, mode=None):
         if mode is None:
             mode = not self._enabled
@@ -44,14 +105,20 @@ class Conv2DViewer:
         self._enabled = bool(mode)
         return old_mode
 
+    def register(self, label, module):
+        pass
+
+    def exit(self):
+        self.close()
+
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.draw()
 
-    def exit(self):
-        self.close()
+    def __del__(self):
+        self.exit()
 
     def __iter__(self):
         return []
