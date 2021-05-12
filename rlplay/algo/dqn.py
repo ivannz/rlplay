8a75707f9a94835d4000cb32136acb773780f38f
Ivan Nazarov <ivannz@yandex.ru>
2021-05-12 12:52:55 +0300

    improved docs; handle target=None properly

--- rlplay/algo/dqn.py
+++ rlplay/algo/dqn.py
@@ -7,24 +7,72 @@ from torch.nn.functional import smooth_l1_loss
 def loss(batch, *, module, target, gamma=0.95, double=True, weights=None):
     r"""Compute the Double-DQN loss.
 
+    Parameters
+    ----------
+    batch : dict of tensor
+        The batch of state-action-reward transitions $
+            s_t, a_t, r_{t+1}, s_{t+1}, T_{t+1}
+        $ where $
+            s_{t+1}, r_{t+1} \sim p(s, r \mid s_t, a_t)
+        $ and $T_{t+1}$ indicates whether $s_{t+1}$ is terminal.
+
+    module : torch.nn.Module
+        The current Q-network.
+
+    target : torch.nn.Module or None
+        The target Q-network. If `None` then the current network is used and
+        in this case `double` is forced to `False`.
+        No backprop takes place via the target network.
+
+    gamma : float, default=0.95
+        The discount factor in the reward stream present value. Must be in
+        `[0, 1)`.
+
+    double : bool, default=True
+        Whether to use Double DQN or not. See details.
+
+    weights : torch.Tensor or None
+        The weight associated with each transition in the batch. Assumed uniform
+        if `None`.
+
     Details
     -------
-    The DQN-target is given by $
-        r_t + \gamma \max_a Q(s_{t+1}, a; \theta^-)
-    $ where $\theta^-$ are frozen parameters of the Q-network (`target`).
-    Now, the D-DQN algorithm of
+    In Q-learning the action value function minimizes the TD-error
+    $$
+        r_{t+1} + \gamma v^*(s_{t+1}) 1_{\neg T_{t+1}}
+            - Q(s_t, a_t; \theta)
+        \,, $$
+    w.r.t. Q-network parameters $\theta$. If classic Q-learning there is no
+    target network and the next state optimal value function is bootstrapped
+    using the current Q-network (`module`):
+    $$
+        v^*(s_{t+1})
+            \approx \max_a Q(s_{t+1}, a; \theta)
+        \,. $$
+    The DQN method, proposed by
+        [Minh et al. (2013)](https://arxiv.org/abs/1312.5602),
+    uses a secondary Q-network to estimate the value of the next state:
+    $$
+        v^*(s_{t+1})
+            \approx \max_a Q(s_{t+1}, a; \theta^-)
+        \,, $$
+    where $\theta^-$ are frozen parameters of the Q-network (`target`). The
+    Double DQN algorithm of
         [van Hasselt et al. (2015)](https://arxiv.org/abs/1509.06461)
     unravels the $\max$ operator as $
-        \max_k v_k
-            \equiv v_{\arg \max_k v_k}
-    $ and replaces the outer $v$ with the Q-values of the target net, while
-    the inner $v$ (inside the $\arg \max$) is given by the Q-values of the
-    under the current policy (`module`). Specifically, the Double DQN-target
-    is $
-        r_t + \gamma Q(s_{t+1}, \hat{a}_{t+1}; \theta^-)
-    $ for $
-        \hat{a}_{t+1} = \arg \max_a Q(s_{t+1}, a; \theta)
-    $ being the action taken by the current `\theta` at $s_{t+1}$.
+        \max_k u_k
+            \equiv u_{\arg \max_k u_k}
+    $ and replaces the outer $u$ with the Q-values of the target net, while
+    the inner $u$ (inside the $\arg\max$) is computed with the Q-values of the
+    current Q-network. Specifically, the Double DQN value estimate is
+    $$
+        v^*(s_{t+1})
+            \approx Q(s_{t+1}, \hat{a}_{t+1}; \theta^-)
+        \,, $$
+    for $
+        \hat{a}_{t+1}
+            = \arg \max_a Q(s_{t+1}, a; \theta)
+    $ being the action taken by the current Q-network $\theta$ at $s_{t+1}$.
 
     Replay Buffer
     -------------
@@ -40,8 +88,14 @@ def loss(batch, *, module, target, gamma=0.95, double=True, weights=None):
     `weights` is expected to be None or a Tensor of shape [batch] of `float32`
     """
 
+    if target is None:
+        # use Q(\cdot; \theta) instead of Q(\cdot; \theta^-)
+        target, double = module, False
+
     # get Q(s_t, a_t; \theta)
     q_replay = module(batch['state']).gather(-1, batch['action'].unsqueeze(-1))
+
+    # make sure no grads are computed for the target Q-value
     with torch.no_grad():
         # get Q(s_{t+1}, \cdot; \theta^-)
         q_target = target(batch['state_next'])
