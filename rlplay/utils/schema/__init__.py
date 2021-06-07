55ce702968ec2a3128c612fea2afea475bdc0dd2
Ivan Nazarov <ivannz@yandex.ru>
2021-06-07 22:26:53 +0300

    minimal exports

--- rlplay/utils/schema/__init__.py
+++ rlplay/utils/schema/__init__.py
@@ -1,4 +1,2 @@
-from .schema import empty_like, method
-from .schema import dtype, astype, shape  # names are too generic
-from .schema import apply_single, getitem, setitem
-from .schema import infer_dtype, check_dtype, rebuild
+from .base import apply_single, apply_pair
+from .tools import astype
