d8d3251abf7a419404b298595b794f525502c073
Ivan Nazarov <ivannz@yandex.ru>
2021-06-22 13:51:32 +0300

    smarter torch.mutlinomial and a proto batchifier

--- rlplay/utils/common.py
+++ rlplay/utils/common.py
@@ -3,6 +3,32 @@ import time
 
 import torch
 
+from functools import wraps
+
+
+def batchify(fn, at):
+    # at is the first dim after the batch
+    @wraps(fn)
+    def _decorator(main, *tensors, **kwargs):
+        ld = at + main.ndim if at < 0 else at
+
+        # *leading, feature@ld, *spatial
+        output = fn(main.flatten(0, ld - 1), *tensors, **kwargs)
+        return output.reshape(*main.shape[:ld], *output.shape[1:])
+
+    return _decorator
+
+
+@wraps(torch.multinomial)
+def multinomial(
+    input, num_samples=1, replacement=False, *, generator=None, out=None
+):
+    out = torch.multinomial(input.flatten(0, -2), num_samples, out=out,
+                            replacement=replacement, generator=generator)
+    if num_samples > 1:
+        return out.reshape(input.shape[:-1] + (num_samples,))
+    return out.reshape(input.shape[:-1])
+
 
 def linear(t, t0=0, t1=100, v0=1., v1=0.):
     tau = min(1., max(0., (t1 - t) / (t1 - t0)))
