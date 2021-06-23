617f5b99f5d15e7142bec39e2b47a7fabd6df43d
ivannz <ivannz@yandex.ru>
2021-06-23 19:34:30 +0300

    extended description of the worker-collector lockstep design, refactored some preparation logic

--- stage/test_sampler.py
+++ stage/test_sampler.py
@@ -1,6 +1,8 @@
 import torch
 import numpy
 
+from collections import namedtuple
+
 from rlplay.engine.collect import prepare, startup, collect
 from rlplay.engine.collect import BaseActorModule
 from rlplay.engine.returns import np_compute_returns
@@ -12,12 +14,19 @@ from rlplay.utils.common import multinomial
 import time
 import gym
 import nle
-from rlplay.utils.vec import CloudpickleSpawner
+from rlplay.engine.vec import CloudpickleSpawner
 
+import torch.multiprocessing as mp
 from threading import BrokenBarrierError
+
+from copy import deepcopy
+
+from functools import wraps
 
 
 class Actor(BaseActorModule):
+    # We should not store references to instantiated env,
+    # since it is about to be communicated to child processes
     def __init__(self, observation, action):
         super().__init__()
 
@@ -57,6 +66,8 @@ class SimpleActor(BaseActorModule):
         self.observation_space, self.action_space = observation, action
         self.emb_obs = torch.nn.Embedding(observation.n, 1 + action.n)
 
+        self.register_buffer('_counter', torch.zeros(1))
+
     def forward(self, obs, act=None, rew=None, fin=None, *, hx=None):
         # Everything is  [T x B x ...]
         n_steps, n_env, *_ = obs.shape
@@ -74,107 +85,233 @@ class SimpleActor(BaseActorModule):
         return actions, (), dict(value=value, logits=logits)
 
 
-def worker_double(factory, actors, buffers, sticky, pin_memory, lockstep):
-    # * receive `factory`, `actors`, `buffers`, `sticky`, and `pin_memory`
+def update_actor(learner, actor):
+    return actor.load_state_dict(learner.state_dict())
+
+
+@wraps(update_actor)
+def locked_update_actor(learner, actor):
+    # disable rollout collection during actor updates
+    with actor.state_dict_lock_:
+        return update_actor(learner, actor)
+
+
+@wraps(collect)
+def locked_collect(envs, actor, fragment, state, *, sticky=False):
+    # disable actor updates during a rollout collection
+    with actor.state_dict_lock_:
+        return collect(envs, actor, fragment, state, sticky=sticky)
+
+
+WorkerContext = namedtuple('WorkerContext', ['envs', 'actor', 'fragment', 'state'])
+
+
+def worker_context(factory, actor, buffer, *, pin_memory=False):
+    # assert not actor.training
+
+    # initialize the environments
+    n_steps, n_envs = buffer.state.fin.shape
+    envs = [factory() for _ in range(n_envs)]
+
+    # prepare the running state and the output buffer
+    state, fragment = startup(envs, actor, buffer, pinned=pin_memory)
+
+    return WorkerContext(envs, actor, fragment, state)
+
+
+def worker_double(barrier, factory, actors, buffers, *,
+                  pin_memory=False, locking=False, sticky=False):
     assert len(buffers) == 2
 
     # initialize the environments
-    context = []
-    for buffer, actor in zip(buffers, actors):
-        n_steps, n_envs = buffer.state.fin.shape
-        envs = [factory() for _ in range(n_envs)]
-
-        # prepare the running state and the output buffer
-        state, fragment = startup(envs, actor, buffer, pinned=pin_memory)
-        context.append((envs, actor, fragment, state))
-
-    # do some preparations
-    j, flipflop = 0, 0
+    context = [worker_context(factory, actor, buffer, pin_memory=pin_memory)
+               for actor, buffer in zip(actors, buffers)]
 
     # the bool return value of `collect` controls the loop
-    while collect(*context[flipflop], sticky=sticky):
+    j, flipflop = 0, 0
+    fn = locked_collect if locking else collect
+    while fn(*context[flipflop], sticky=sticky):
         # switch to the next fragment buffer
         j, flipflop = j + 1, 1 - flipflop
 
         # indicate that the previous fragment is ready
         try:
-            lockstep.wait()
+            barrier.wait()
 
         except BrokenBarrierError:
             # our parent breaks the barrier if it wants us to shutdown
             break
 
-    else:  # branch here only if the while loop was NOT broken out of.
-        # `collect` aborted the while-loop, let the parent know something went
-        # wrong by breaking the barrier.
-        lockstep.abort()
+    # this branch is visited only if the while clause evaluates to False, and
+    # never when the loop is broken out of.
+    else:
+        # `collect` aborted the while-loop, let the parent know that something
+        # went wrong with the current buffer by breaking the barrier.
+        barrier.abort()
 
     # close the environments in states
-    pass
+    for ctx in context:
+        for env in ctx.envs:
+            env.close()
+
+    return True
+
+
+CollectorContext = namedtuple('CollectorContext', ['actor', 'buffer'])
+
+
+def new_actor(learner, *, update_lock=True):
+    # make a deepcopy of the learner (obv. synchronizes weights)
+    actor = deepcopy(learner)
+
+    # set evaluation mode and move to shared memory (in-place)
+    actor.share_memory()  # .eval()
+
+    # the actor has a shared lock, which mutexes state dict updates
+    if update_lock:
+        actor.state_dict_lock_ = mp.Lock()
+    # XXX not needed if we use actor duplication
+
+    return actor
+
+
+def collector_contexts(env, learner, n_steps, n_envs, *, double=True):
+    # the actors are stale copies of the learner (parameter-wise)
+    actor = new_actor(learner, update_lock=not double)
+    actor_alt = new_actor(learner, update_lock=not double) if double else actor
+    actors = actor, actor_alt
+
+    # create aliased trajectory fragment buffers in the shared memory
+    buffers = [aliased(prepare(env, actor, n_steps, n_envs, shared=True))
+               for actor in actors]
+
+    # bundle actor and aliased fragment together for faster switching
+    return [CollectorContext(a, f) for a, f in zip(actors, buffers)]
 
 
 def collector_double(factory, n_steps, n_envs):
-    # * receive `factory`, `n_steps`, and `n_envs`
-    import torch.multiprocessing as mp
+    r"""
 
-    sticky, pin_memory = False, False
+    Details
+    -------
+    We use double buffering approach with an independent actor per buffer. This
+    setup allows for truly parallel collection and learning. The worker (W) and
+    the learner (L) move in lockstep: W is collecting rollout into one buffer,
+    while L is training on the other buffer. This tandem movement is achieved
+    via barrier synchronisation.
 
-    # initialize a sample environment and our main actor
+        +--------------------+           +--------------------+
+        | worker process     |  sharing  | learner process    |     sync
+        +--------------------+           +--------------------+
+        |                    |           | wait for the first |
+        | collect(a_1, b_1)  |           | buffer to be ready |
+        |                    |           |                    |
+        +====================+           +====================+  <- barrier
+        |                    ---- b_1 -->> train(l, b_1) ---+ |
+        | collect(a_2, b_2)  |           |                  | |
+        |                    <<-- a_1 ---- update(l, a_1) <-+ |
+        +====================+           +====================+  <- barrier
+        |                    ---- b_2 -->> train(l, b_2) ---+ |
+        | collect(a_1, b_1)  |           |                  | |
+        |                    <<-- a_2 ---- update(l, a_2) <-+ |
+        +====================+           +====================+  <- barrier
+        |                    ---- b_1 -->> train(l, b_1) ---+ |
+        | collect(a_2, b_2)  |           |                  | |
+        |                    <<-- a_1 ---- update(l, a_1) <-+ |
+        +====================+           +====================+  <- barrier
+        |    ...      ...    |           |    ...      ...    |
+
+    The dual-actor setup eliminates worker's post-collection idle time, that
+    exists in a single-actor setup, which stems from the need of the learner
+    to acquire a lock on that single actor's state-dict. Although state-dict
+    updates via torch's shared memory storage are fast, so the idle period is
+    relatively short. At the same time it takes twice as much memory, since
+    we have to maintain TWO instances of the actor model.
+
+    Single-actor setup (assuming non-negligible `train` time).
+
+        +--------------------+           +--------------------+
+        | worker process     |  sharing  | learner process    |     sync
+        +--------------------+           +--------------------+
+        | with a.lock:       |           | wait for the first |
+        |   collect(a, b_1)  |           | buffer to be ready |
+        |                    |           |                    |
+        +====================+           +====================+  <- barrier
+        | with a.lock:       ---- b_1 -->> train(l, b_1) ---+ |
+        |   collect(a, b_2)  |           |                  | |
+        |                    |           | with a.lock:     | |
+        | # idle time        <<--  a  ----   update(l, a) <-+ |
+        +====================+           +====================+  <- barrier
+        | with a.lock:       ---- b_2 -->> train(l, b_2) ---+ |
+        |   collect(a, b_1)  |           |                  | |
+        |                    |           | with a.lock:     | |
+        | # idle time        <<--  a  ----   update(l, a) <-+ |
+        +====================+           +====================+  <- barrier
+        | with a.lock:       ---- b_1 -->> train(l, b_1) ---+ |
+        |   collect(a, b_2)  |           |                  | |
+        |                    |           | with a.lock:     | |
+        | # idle time        <<--  a  ----   update(l, a) <-+ |
+        +====================+           +====================+  <- barrier
+        |    ...      ...    |           |    ...      ...    |
+
+    """
+    sticky, pin_memory, double = False, False, True
+
+    # initialize a sample environment and our learner model instance
     env = factory()
-
-    # the actor should not store references to instantiated env, since it is
-    # about to be communicated to child processes.
-    actors = [SimpleActor(env.observation_space, env.action_space)
-              for _ in range(2)]
-
     learner = SimpleActor(env.observation_space, env.action_space)
 
-    # create an aliased shared buffer for trajectory fragments
-    buffers = []
-    for actor in actors:
-        # prepare the trajectory fragment buffer in the shared memory
-        buffer = prepare(env, actor, n_steps, n_envs // 2, shared=True)
-        actor.load_state_dict(learner.state_dict())
-
-        # bundle actor and aliased fragment together for faster switching
-        buffers.append((actor.share_memory(), aliased(buffer)))
-
-    del env, buffer
+    # create two contexts for double buffering
+    contexts = collector_contexts(env, learner, n_steps, n_envs, double=double)
+    del env
 
     # prepare the optimizer for the learner
     optim = torch.optim.Adam(learner.parameters(), lr=1e-1)
 
     # print(unsafe_apply(fragment, fn=lambda x: x.shape))
     # we use `.pyt`, since torch has simpler pickling/unpickling for sharing
-    actors, fragments_pyt = zip(*((a, f.pyt) for a, f in buffers))
+    actors, fragments_pyt = zip(*((ctx.actor, ctx.buffer.pyt)
+                                  for ctx in contexts))
 
-    # spawn the worker process
-    lockstep = mp.Barrier(2)
-    p_worker = mp.Process(target=worker_double, args=(
-        factory, actors, fragments_pyt, sticky, pin_memory, lockstep
-    ), daemon=False)
+    # the double buffering barrier for syncing interleaving iterations
+    barrier = mp.Barrier(2)
+    p_worker = mp.Process(
+        target=worker_double, group=None, name=None, daemon=False,
+        args=(barrier, factory, actors, fragments_pyt),
+        kwargs=dict(sticky=sticky, pin_memory=pin_memory, locking=not double),
+    )
     p_worker.start()
 
-    # we use double buffering
+    # use proper update mechanism
+    update = locked_update_actor if not double else update_actor
+
+    # we use double buffering: the code flow is designed and the barriers set
+    # up in such a way, that flipflop becomes synchronized between this and the
+    # worker processes.
     terminate, j, flipflop = False, 0, 0
     while not terminate:
         # wait for the current fragment to be ready (w.r.t `flipflop`)
         try:
-            lockstep.wait()
+            barrier.wait()
 
         except BrokenBarrierError:
             # the worker broke the barrier to indicate emergency shutdown
             break
 
         # get the current fragment and the actor used to collect it
-        terminate = not do_stuff(j, learner, optim, *buffers[flipflop])
+        ctx = contexts[flipflop]
+        terminate = not do_stuff(j, learner, optim, *ctx)
+
+        # updating the shared actor should not be followed by heavy
+        #  computations or any io-bound tasks (aside from itself)
+        update(learner, ctx.actor)
 
         # switch to the next buffer
         j, flipflop = j + 1, 1 - flipflop
 
     else:
         # we break the barrier, if we wish to shut down the worker
-        lockstep.abort()
+        barrier.abort()
 
     p_worker.join()
 
@@ -182,6 +319,7 @@ def collector_double(factory, n_steps, n_envs):
     print(catted)
     print(all(unsafe_apply(catted, fn=lambda x: (yield x.is_shared()))))
     print(unsafe_apply(catted, fn=lambda x: x.shape))
+    print(learner._counter)
 
 
 def do_stuff(j, module, optim, actor, fragment):
@@ -209,16 +347,10 @@ def do_stuff(j, module, optim, actor, fragment):
     (1e-1 * ell2 + 1e-2 * negent).backward()
     optim.step()
 
-    # update the shared actor
-    actor.load_state_dict(module.state_dict())
+    # time.sleep(0.05)
+    # pass
 
-    # time.sleep(0.25)
-    pass
-
-    # print(j, crew, fragment.npy.bootstrap)
-    # print(j, float(negent))
-
-    return j < 300  # return True
+    return j < 1200  # return True
 
 
 if __name__ == '__main__':
@@ -246,7 +378,6 @@ if __name__ == '__main__':
         # return gym.make('SpaceInvadersNoFrameskip-v4').unwrapped
         # return RandomDiscoMaze(10, 10, field=(2, 2))
         return RandomDiscoMazeWithPosition(10, 10, field=(2, 2))
-
 
     collector_double(CloudpickleSpawner(factory), T, B)
 
