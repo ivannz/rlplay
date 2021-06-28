7354f353bcd0bfb2133d06ea1ae10ab71bbe99c8
ivannz <ivannz@yandex.ru>
2021-06-28 21:57:42 +0300

    deltas, v-trace and unit tests

--- rlplay/engine/returns.py
+++ rlplay/engine/returns.py
@@ -3,24 +3,59 @@ import torch
 
 
 # returns, baselined or not, or advantage estimates are not diff-able in PG
-def npy_returns(rew, fin, *, gamma, bootstrap=0.):
+def npy_returns(rew, fin, *, gamma, bootstrap=0., omega=None, r_bar=None):
     r"""Compute the on-policy returns (the present value of the future rewards).
 
-        G_t = r_{t+1} + \gamma r_{t+2} + \gamma^2 r_{t+3} + ...
-            = r_{t+1} + \gamma G_{t+1}
+        G_t = r_{t+1}
+              + \gamma \omega_{t+1} r_{t+2}
+              + \gamma^2 \omega_{t+1} \omega_{t+2} r_{t+3} + ...
+            = \sum_{j\geq t} r_{j+1} \gamma^{j-t} \prod_{s=t+1}^j \omega_s
+            = r_{t+1} + \gamma \omega_{t+1} G_{t+1}
     """
+    if omega is not None:
+        # \rho_t = \min\{ \bar{\rho}, \frac{\pi_t(a_t)}{\mu_t(a_t)} \}
+        rho = numpy.minimum(numpy.exp(omega), r_bar or float('+inf'))
+
     n_steps, *shape = rew.shape
     G_t = numpy.zeros((1 + n_steps, *shape), dtype=rew[-1].dtype)
 
     # rew[t], fin[t] is r_{t+1} and d_{t+1}
     G_t[-1] = bootstrap
     for j in range(1, n_steps + 1):
-        # get G_t = r_{t+1} + \gamma G_{t+1} 1_{\neg d_{t+1}}
+        # get G_t = r_{t+1} + \gamma \rho_t G_{t+1} 1_{\neg d_{t+1}}
         # XXX G_t[-j-1] is all zeros
         numpy.multiply(G_t[-j], gamma, where=~fin[-j], out=G_t[-j-1])
+        if omega is not None:
+            G_t[-j-1] *= rho[-j]
+
         G_t[-j-1] += rew[-j]
 
     return G_t[:-1]
+
+
+def npy_deltas(rew, fin, val, *, gamma, bootstrap=0., omega=None, r_bar=None):
+    r"""Compute the importance weighted td-error estimates:
+
+        \delta_t = r_{t+1} + \gamma v(s_{t+1}) 1_{\neg d_{t+1}} - v(s_t)
+        # \delta^v_s = 0 for all s \geq t if d_t = \top
+    """
+    n_steps, *shape = rew.shape
+
+    a_hat = numpy.zeros_like(rew)
+    a_hat[-1:] = bootstrap
+    a_hat[:-1] = val[1:]
+
+    # \delta_t = r_{t+1} + \gamma v(s_{t+1}) 1_{\neg d_{t+1}} - v(s_t)
+    numpy.putmask(a_hat, fin, 0.)
+    a_hat *= gamma
+    a_hat += rew
+    a_hat -= val
+
+    if omega is not None:
+        # \rho_t = \min\{ \bar{\rho}, \frac{\pi_t(a_t)}{\mu_t(a_t)} \}
+        rho = numpy.minimum(numpy.exp(omega), r_bar or float('+inf'))
+        a_hat *= rho
+    return a_hat
 
 
 def npy_gae(rew, fin, val, *, gamma, C, bootstrap=0.):
@@ -57,7 +92,7 @@ def npy_gae(rew, fin, val, *, gamma, C, bootstrap=0.):
     return gae_t[:-1]
 
 
-def npy_vtrace(rew, fin, val, omega, *, gamma, bootstrap, r_bar, c_bar):
+def npy_vtrace(rew, fin, val, omega, *, gamma, r_bar, c_bar, bootstrap=0.):
     r"""Compute the V-trace value estimates ($n \to \infty$ limit):
 
         \delta^v_t = r_{t+1} + \gamma v(s_{t+1}) 1_{\neg d_{t+1}} - v(s_t)
@@ -81,52 +116,94 @@ def npy_vtrace(rew, fin, val, omega, *, gamma, bootstrap, r_bar, c_bar):
             \rho_j = \min\{e^\omega_j, \bar{\rho} \}
         $, $\omega_t = \log \pi(a_t \mid x_t) - \log \mu(a_t \mid x_t)$, and
         $\mu$ is the behavior policy, while $\pi$ is the target policy.
+
+        Let $
+            \hat{a}_t := \hat{v}^\infty_{t+1} - v(s_{t+1}
+        $, then
+        \hat{a}_t
+            = \rho_t \delta^v_t
+            + \gamma c_t \hat{a}_{t+1} 1_{\neg d_{t+1}}
     """
-    raise NotImplementedError
+    # clamp(max=a) is the same is min(..., a)
+    rho = numpy.minimum(numpy.exp(omega), r_bar or float('+inf'))
+    see = numpy.minimum(numpy.exp(omega), c_bar or float('+inf'))
 
     # V-trace uses importance weights to correct for off-policy PG
-    n_steps, shape = len(rew), rew[-1].shape
+    n_steps, *shape = rew.shape
 
-    v_hat = numpy.zeros((1 + n_steps, *shape), dtype=rew[-1].dtype)
+    a_hat = numpy.zeros((1 + n_steps, *shape), dtype=rew[-1].dtype)
     delta = numpy.zeros(shape, dtype=rew[-1].dtype)
+
     # rew[t], fin[t], val[t] is r_{t+1}, d_{t+1} and v(s_t)
     # t is -j, t+1 is -j-1 (j=1..T)
     for j in range(1, n_steps + 1):
-        # \delta_t = r_{t+1} + \gamma v(s_{t+1}) 1_{\neg d_{t+1}} - v(s_t)
+        # \rho_t \bigl( r_{t+1} + \gamma v(s_{t+1}) 1_{\neg d_{t+1}} - v(s_t) \bigr)
         numpy.multiply(bootstrap, gamma, out=delta, where=~fin[-j])
         delta += rew[-j] - val[-j]
+        delta *= rho[-j]
 
-        # A_t = \delta_t + \lambda \gamma A_{t+1} 1_{\neg d_{t+1}}
-        numpy.multiply(v_hat[-j], C * gamma, out=v_hat[-j-1], where=~fin[-j])
-        v_hat[-j-1] += delta
+        # A_t = \rho_t \delta_t + \c_t \gamma A_{t+1} 1_{\neg d_{t+1}}
+        numpy.multiply(a_hat[-j], gamma, out=a_hat[-j-1], where=~fin[-j])
+        a_hat[-j-1] *= see[-j]
+        a_hat[-j-1] += delta
 
         # reset delta for the next conditional multiply
         bootstrap = val[-j]  # v(s_t) is next iter's bootstrap
         delta[:] = 0
 
-    return v_hat[:-1]
+    return a_hat[:-1] + val
 
 
 @torch.no_grad()
-def pyt_returns(rew, fin, *, gamma, bootstrap=0.):
-    n_steps, *shape = rew.shape
+def pyt_returns(rew, fin, *, gamma, bootstrap=0., omega=None, r_bar=None):
+    r"""Compute the importance weighted present-value estimate:
 
-    # G_t = v(s_t) = r_{t+1} + \gamma G_{t+1} 1_{\neg T_{t+1}}
+        G_t = r_{t+1} + \gamma \rho_t G_{t+1} 1_{\neg d_{t+1}}
+    """
+    # v(s_t) ~ G_t = r_{t+1} + \gamma G_{t+1} 1_{\neg d_{t+1}}
     # r_{t+1}, s_{t+1} \sim p(r, s, \mid s_t, a_t), a_t \sim \pi(a \mid s_t)
-    # T_{t+1} indicates if $s_{t+1}$ is terminal
+    # d_{t+1} indicates if $s_{t+1}$ is terminal
+    if omega is not None:
+        # \rho_t = \min\{ \bar{\rho}, \frac{\pi_t(a_t)}{\mu_t(a_t)} \}
+        rho = omega.exp().clamp_(max=r_bar or float('+inf'))
+
+    n_steps, *shape = rew.shape
     G_t = rew.new_zeros((1 + n_steps, *shape))
 
     G_t[-1].copy_(bootstrap)  # bootstrap of \approx (r_{H+k+1})_{k\geq 0}
     for j in range(1, n_steps + 1):
-        # compute $G_t = r_{t+1} + \gamma G_{t+1}$ for t = L - j
-        # NB G[-j] is G_{t+1} and G[-j-1] is G_t
+        # G_t = \rho_t \delta_t + \gamma \rho_t G_{t+1} 1_{\neg d_{t+1}}
+        # XXX G[-j] is G_{t+1} and G[-j-1] is G_t, and G_t[-j-1] is all zeros
+        if omega is not None:
+            G_t[-j-1].addcmul_(G_t[-j], rho[-j], value=gamma)
+        else:
+            G_t[-j-1].add_(G_t[-j], alpha=gamma)
 
-        # get 1_{\neg T_{t+1}} v(s_{t+1}) \gamma + r_{t+1}
-        G_t[-j-1].copy_(G_t[-j]).mul_(gamma)  # get \hat{G}_{t+1}(\tau) \gamma
-        G_t[-j-1].masked_fill_(fin[-j], 0.)  # fin[-j] means s_{t+1} is terminal
+        G_t[-j-1].masked_fill_(fin[-j], 0.)
         G_t[-j-1].add_(rew[-j])  # add the received reward r_{t+1}
 
     return G_t[:-1]
+
+
+@torch.no_grad()
+def pyt_deltas(rew, fin, val, *, gamma, bootstrap=0., omega=None, r_bar=None):
+    r"""Compute the importance weighted td-error estimates:
+
+        \delta_t = r_{t+1} + \gamma v(s_{t+1}) 1_{\neg d_{t+1}} - v(s_t)
+        # \delta^v_s = 0 for all s \geq t if d_t = \top
+    """
+    # a_hat[t] = val[t+1]
+    a_hat = torch.empty_like(rew).copy_(bootstrap)
+    a_hat[:-1].copy_(val[1:])
+
+    # \delta_t = r_{t+1} + \gamma v(s_{t+1}) 1_{\neg d_{t+1}} - v(s_t)
+    a_hat.masked_fill_(fin, 0.).mul_(gamma).add_(rew).sub_(val)
+    if omega is None:
+        return a_hat
+
+    # \rho_t = \min\{ \bar{\rho}, \frac{\pi_t(a_t)}{\mu_t(a_t)} \}
+    rho = omega.exp().clamp_(max=r_bar or float('+inf'))
+    return a_hat.mul_(rho)
 
 
 @torch.no_grad()
@@ -138,19 +215,44 @@ def pyt_gae(rew, fin, val, *, gamma, C, bootstrap=0.):
     # t is -j, t+1 is -j-1 (j=1..T)
     for j in range(1, n_steps + 1):
         # \delta_t = r_{t+1} + \gamma v(s_{t+1}) 1_{\neg d_{t+1}} - v(s_t)
-        delta.copy_(bootstrap).mul_(gamma)
-        delta.masked_fill_(fin[-j], 0.)
-        bootstrap = val[-j]  # v(s_t) is next iter's bootstrap
-        delta.add_(rew[-j]).sub_(bootstrap)  # add r_{t+1} - v(s_t)
+        delta.add_(bootstrap, alpha=gamma).masked_fill_(fin[-j], 0.)
+        delta.add_(rew[-j]).sub_(val[-j])  # add r_{t+1} - v(s_t)
 
         # A_t = \delta_t + \lambda \gamma A_{t+1} 1_{\neg d_{t+1}}
-        gae_t[-j-1].copy_(gae_t[-j]).mul_(C * gamma)
-        gae_t[-j-1].masked_fill_(fin[-j], 0.)
+        gae_t[-j-1].add_(gae_t[-j], alpha=C * gamma).masked_fill_(fin[-j], 0.)
         gae_t[-j-1].add_(delta)
+
+        bootstrap = val[-j]  # v(s_t) is next iter's bootstrap
+        delta.zero_()
 
     return gae_t[:-1]
 
 
 @torch.no_grad()
-def pyt_vtrace(rew, fin, val, omega, *, gamma, bootstrap, r_bar, c_bar):
-    raise NotImplementedError
+def pyt_vtrace(rew, fin, val, *, gamma, bootstrap=0., omega=None, r_bar, c_bar):
+    # raise NotImplementedError
+    n_steps, *shape = rew.shape
+
+    # \rho_t = \min\{ \bar{\rho},  \frac{\pi_t(a_t)}{\mu_t(a_t)} \}
+    rho = omega.exp().clamp_(max=r_bar or float('+inf'))
+
+    # c_t = \min\{ \bar{c},  \frac{\pi_t(a_t)}{\mu_t(a_t)} \}
+    see = omega.exp().clamp_(max=c_bar or float('+inf'))
+
+    a_hat, delta = rew.new_zeros((1 + n_steps, *shape)), rew.new_zeros(shape)
+    # rew[t], fin[t], val[t] is r_{t+1}, d_{t+1} and v(s_t)
+    # t is -j, t+1 is -j-1 (j=1..T)
+    for j in range(1, n_steps + 1):
+        # \delta_t = r_{t+1} + \gamma v(s_{t+1}) 1_{\neg d_{t+1}} - v(s_t)
+        delta.add_(bootstrap, alpha=gamma).masked_fill_(fin[-j], 0.)
+        delta.add_(rew[-j]).sub_(val[-j])  # add r_{t+1} - v(s_t)
+
+        # A_t = \rho_t \delta_t + \gamma \c_t A_{t+1} 1_{\neg d_{t+1}}
+        a_hat[-j-1].addcmul_(a_hat[-j], see[-j], value=gamma)
+        a_hat[-j-1].masked_fill_(fin[-j], 0.)
+        a_hat[-j-1].addcmul_(delta, rho[-j])
+
+        bootstrap = val[-j]  # v(s_t) is next iter's bootstrap
+        delta.zero_()
+
+    return a_hat[:-1] + val
--- tests/test_engine_returns.py
+++ tests/test_engine_returns.py
@@ -7,36 +7,130 @@ from collections import namedtuple
 
 from rlplay.utils.schema.shared import aliased
 from rlplay.engine.returns import pyt_gae, npy_gae
+from rlplay.engine.returns import pyt_deltas, npy_deltas
 from rlplay.engine.returns import pyt_returns, npy_returns
+from rlplay.engine.returns import pyt_vtrace, npy_vtrace
 
 RewardsData = namedtuple('RewardsData', [
     'rew', 'fin', 'val', 'bootstrap', 'omega'
 ])
 
 
-def random_reward_data(T=120, B=10):
-    rew, val = torch.randn(T, B), torch.randn(T, B)
+def random_reward_data(T=120, B=10, dtype=float):
+    rew, val = torch.randn(T, B, dtype=dtype), torch.randn(T, B, dtype=dtype)
     fin = torch.randint(2, size=(T, B,), dtype=bool)
-    bootstrap = torch.randn(1, B)
+    bootstrap = torch.randn(1, B, dtype=dtype)
 
     # log importance weight of the taken action (target vs. behavioral)
-    omega = torch.randn(T, B)
+    omega = torch.randn(T, B, dtype=dtype)
     return aliased(RewardsData(rew, fin, val, bootstrap, omega))
+
+
+def npy_manual_present_value(rew, fin, *, gamma, rho, bootstrap):
+    # compute returns
+    results, discounts = [], (~fin) * gamma
+
+    # backward accumulation
+    present = numpy.array(bootstrap, copy=True)
+    for t in range(1, 1 + len(rew)):
+        present = rew[-t] + rho[-t] * discounts[-t] * present
+        results.append(present)
+    results.reverse()
+
+    return numpy.concatenate(results, axis=0)
+
+
+def npy_manual_deltas(rew, fin, val, *, gamma, rho, bootstrap):
+    # get td errors
+    discounts = (~fin) * gamma
+    vtp1 = numpy.concatenate([val[1:], bootstrap], axis=0)
+
+    return rho * (rew + discounts * vtp1 - val)
 
 
 @pytest.mark.parametrize('gamma', [
     0.0, 0.5, 0.9, 0.999, 1.0
 ])
 def test_returns(gamma, T=120, B=10):
-
     data = random_reward_data(T, B)
+
+    expected = npy_manual_present_value(
+        data.npy.rew, data.npy.fin, gamma=gamma,
+        rho=numpy.ones_like(data.npy.rew),
+        bootstrap=data.npy.bootstrap)
+
     npy = npy_returns(data.npy.rew, data.npy.fin,
-                      gamma=gamma, bootstrap=data.npy.bootstrap[0])
+                      gamma=gamma, omega=None,
+                      bootstrap=data.npy.bootstrap[0])
+    assert numpy.allclose(npy, expected)
 
     pyt = pyt_returns(data.pyt.rew, data.pyt.fin,
-                      gamma=gamma, bootstrap=data.pyt.bootstrap[0])
+                      gamma=gamma, omega=None,
+                      bootstrap=data.pyt.bootstrap[0])
+    assert numpy.allclose(pyt.numpy(), expected)
 
-    assert numpy.allclose(pyt.numpy(), npy)
+    # test importance weights
+    for r_bar in [0.5, 1.0, 2.0, None]:
+        rho = numpy.minimum(numpy.exp(data.npy.omega), r_bar or float('+inf'))
+
+        expected = npy_manual_present_value(
+            data.npy.rew, data.npy.fin, gamma=gamma,
+            rho=rho, bootstrap=data.npy.bootstrap)
+
+        omega = numpy.log(rho)
+
+        npy = npy_returns(
+            data.npy.rew, data.npy.fin, gamma=gamma, r_bar=None,
+            omega=omega, bootstrap=data.npy.bootstrap[0])
+        assert numpy.allclose(npy, expected)
+
+        pyt = pyt_returns(
+            data.pyt.rew, data.pyt.fin, gamma=gamma, r_bar=None,
+            omega=torch.from_numpy(omega), bootstrap=data.pyt.bootstrap[0])
+
+        assert numpy.allclose(pyt.numpy(), expected)
+
+
+@pytest.mark.parametrize('gamma', [
+    0.0, 0.5, 0.9, 0.999, 1.0
+])
+def test_deltas(gamma, T=120, B=10):
+    data = random_reward_data(T, B)
+
+    expected = npy_manual_deltas(
+        data.npy.rew, data.npy.fin, data.npy.val, gamma=gamma,
+        rho=numpy.ones_like(data.npy.rew),
+        bootstrap=data.npy.bootstrap)
+
+    npy = npy_deltas(data.npy.rew, data.npy.fin, data.npy.val,
+                     gamma=gamma, bootstrap=data.npy.bootstrap[0])
+    assert numpy.allclose(npy, expected)
+
+    pyt = pyt_deltas(data.pyt.rew, data.pyt.fin, data.pyt.val,
+                     gamma=gamma, bootstrap=data.pyt.bootstrap[0])
+
+    assert numpy.allclose(pyt.numpy(), expected)
+
+    # test importance weights
+    for r_bar in [0.5, 1.0, 2.0, None]:
+        rho = numpy.minimum(numpy.exp(data.npy.omega), r_bar or float('+inf'))
+
+        expected = npy_manual_deltas(
+            data.npy.rew, data.npy.fin, data.npy.val, gamma=gamma,
+            rho=rho, bootstrap=data.npy.bootstrap)
+
+        omega = numpy.log(rho)
+
+        npy = npy_deltas(
+            data.npy.rew, data.npy.fin, data.npy.val, gamma=gamma, r_bar=None,
+            omega=omega, bootstrap=data.npy.bootstrap[0])
+        assert numpy.allclose(npy, expected)
+
+        pyt = pyt_deltas(
+            data.pyt.rew, data.pyt.fin, data.pyt.val, gamma=gamma, r_bar=None,
+            omega=torch.from_numpy(omega), bootstrap=data.pyt.bootstrap[0])
+
+        assert numpy.allclose(pyt.numpy(), expected)
 
 
 @pytest.mark.parametrize('gamma', [
@@ -48,10 +142,67 @@ def test_returns(gamma, T=120, B=10):
 def test_gae(gamma, C, T=120, B=10):
     data = random_reward_data(T, B)
 
+    # GAE is essentially present value of one-step td-errors
+    deltas = npy_manual_deltas(
+        data.npy.rew, data.npy.fin, data.npy.val,
+        rho=numpy.ones_like(data.npy.rew),
+        gamma=gamma, bootstrap=data.npy.bootstrap)
+
+    expected = npy_manual_present_value(
+        deltas, data.npy.fin,
+        rho=numpy.ones_like(data.npy.rew), gamma=gamma * C,
+        bootstrap=numpy.zeros_like(data.npy.bootstrap))
+
     npy = npy_gae(data.npy.rew, data.npy.fin, data.npy.val,
                   gamma=gamma, C=C, bootstrap=data.npy.bootstrap[0])
+    assert numpy.allclose(npy, expected)
 
     pyt = pyt_gae(data.pyt.rew, data.pyt.fin, data.pyt.val,
                   gamma=gamma, C=C, bootstrap=data.pyt.bootstrap[0])
 
-    assert numpy.allclose(pyt.numpy(), npy, rtol=1e-5, atol=1e-6)
+    assert numpy.allclose(pyt.numpy(), expected)
+
+
+@pytest.mark.parametrize('gamma', [
+    0.0, 0.5, 0.9, 0.999, 1.0
+])
+@pytest.mark.parametrize('r_bar', [
+    0.5, 1.0, 2.0, None
+])
+@pytest.mark.parametrize('c_bar', [
+    0.5, 1.0, 2.0, None
+])
+def test_vtrace(gamma, r_bar, c_bar, T=120, B=10):
+    # gamma, r_bar, c_bar, T, B = 0.5, 1.0, 1.0, 120, 10
+    data = random_reward_data(T, B)
+
+    # compute the vtrace estimates manually
+    ratio = numpy.exp(data.npy.omega)
+    rho = numpy.minimum(ratio, r_bar) if r_bar is not None else ratio
+    see = numpy.minimum(ratio, c_bar) if c_bar is not None else ratio
+
+    # v-trace is essentially GAE with C=1 and different importance weights
+    #  shifted by the value estimates
+    deltas = npy_manual_deltas(
+        data.npy.rew, data.npy.fin, data.npy.val,
+        rho=rho,
+        gamma=gamma, bootstrap=data.npy.bootstrap)
+
+    advantage = npy_manual_present_value(
+        deltas, data.npy.fin,
+        rho=see, gamma=gamma,
+        bootstrap=numpy.zeros_like(data.npy.bootstrap))
+
+    expected = advantage + data.npy.val
+
+    npy = npy_vtrace(
+        data.npy.rew, data.npy.fin, data.npy.val, omega=data.npy.omega,
+        gamma=gamma, r_bar=r_bar, c_bar=c_bar, bootstrap=data.npy.bootstrap[0])
+
+    assert numpy.allclose(npy, expected)
+
+    pyt = pyt_vtrace(
+        data.pyt.rew, data.pyt.fin, data.pyt.val, omega=data.pyt.omega,
+        gamma=gamma, r_bar=r_bar, c_bar=c_bar, bootstrap=data.pyt.bootstrap[0])
+
+    assert numpy.allclose(pyt.numpy(), expected)
