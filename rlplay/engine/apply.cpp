735406f374705daed84c20fc339a13e313a4a093
Ivan Nazarov <ivannz@yandex.ru>
2021-07-08 02:50:37 +0300

    setitem-getitem with kwarg index, to reduce lambda impact; [st][_u]ply suite of apply with preset safe ans star, to eliminate kwarg parsing overhead

--- rlplay/engine/apply.cpp
+++ rlplay/engine/apply.cpp
@@ -308,26 +308,37 @@ static PyObject* _apply(PyObject *callable, PyObject *main, PyObject *rest,
     return result;
 }
 
+
+int parse_apply_args(PyObject *args, PyObject **callable, PyObject **main, PyObject **rest)
+{
+    PyObject *first = PyTuple_GetSlice(args, 0, 2);
+    int parsed = PyArg_ParseTuple(first, "OO|:apply", callable, main);
+    Py_DECREF(first);
+
+    if (!parsed)
+        return 0;
+
+    if(!PyCallable_Check(*callable)) {
+        PyErr_SetString(PyExc_TypeError, "The first argument must be a callable.");
+        return 0;
+    }
+
+    Py_ssize_t len = PyTuple_GET_SIZE(args);
+    *rest = PyTuple_GetSlice(args, 2, len);
+
+    return 1;
+}
+
 static PyObject* apply(PyObject *self, PyObject *args, PyObject *kwargs)
 {
     // from the url at the top: {API 1.2.1} the call mechanism guarantees
     //  to hold a reference to every argument for the duration of the call.
     int safe = 1, star = 1;
-    PyObject *callable = NULL, *main = NULL;
+    PyObject *callable = NULL, *main = NULL, *rest = NULL;
 
-    //handle `apply(fn, main, *rest, *, ...)`
-    Py_ssize_t len = PyTuple_GET_SIZE(args);
-    PyObject *first = PyTuple_GetSlice(args, 0, 2);
-    PyObject *rest = PyTuple_GetSlice(args, 2, len);
-
-    int parsed = PyArg_ParseTuple(first, "OO|:apply", &callable, &main);
-    Py_DECREF(first);
-    if (!parsed) return NULL;
-
-    if(!PyCallable_Check(callable)) {
-        PyErr_SetString(PyExc_TypeError, "The first argument must be a callable.");
+    //handle `apply(fn, main, *rest, ...)`
+    if(!parse_apply_args(args, &callable, &main, &rest))
         return NULL;
-    }
 
     //handle `apply(..., *, _star, _safe, **kwargs)`
     if (kwargs) {
@@ -364,12 +375,120 @@ static PyObject* apply(PyObject *self, PyObject *args, PyObject *kwargs)
     return result;
 }
 
+// apply functions with preset _safe and _star kwargs
+// [ts][u_]apply -- t/s tuple or star args, u/_ unsafe or safe
+static PyObject* suply(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+    PyObject *callable = NULL, *main = NULL, *rest = NULL;
+    if(!parse_apply_args(args, &callable, &main, &rest))
+        return NULL;
+
+    PyObject *result = _apply(callable, main, rest, 0, 1, kwargs);
+    Py_DECREF(rest);
+
+    return result;
+}
+
+static PyObject* tuply(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+    PyObject *callable = NULL, *main = NULL, *rest = NULL;
+    if(!parse_apply_args(args, &callable, &main, &rest))
+        return NULL;
+
+    PyObject *result = _apply(callable, main, rest, 0, 0, kwargs);
+    Py_DECREF(rest);
+
+    return result;
+}
+
+static PyObject* s_ply(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+    PyObject *callable = NULL, *main = NULL, *rest = NULL;
+    if(!parse_apply_args(args, &callable, &main, &rest))
+        return NULL;
+
+    PyObject *result = _apply(callable, main, rest, 1, 1, kwargs);
+    Py_DECREF(rest);
+
+    return result;
+}
+
+static PyObject* t_ply(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+    PyObject *callable = NULL, *main = NULL, *rest = NULL;
+    if(!parse_apply_args(args, &callable, &main, &rest))
+        return NULL;
+
+    PyObject *result = _apply(callable, main, rest, 1, 0, kwargs);
+    Py_DECREF(rest);
+
+    return result;
+}
+
+static PyObject *getitem(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+    static char *kwlist[] = {"", "index", NULL};
+
+    PyObject *object = NULL, *index = NULL;
+    int parsed = PyArg_ParseTupleAndKeywords(args, kwargs, "O|$O:getitem",
+                                             kwlist, &object, &index);
+    if(!parsed)
+        return NULL;
+
+    return PyObject_GetItem(object, index);
+}
+
+static PyObject * setitem(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+    static char *kwlist[] = {"", "", "index", NULL};
+
+    PyObject *object = NULL, *value = NULL, *index = NULL;
+    int parsed = PyArg_ParseTupleAndKeywords(
+        args, kwargs, "OO|$O:setitem", kwlist, &object, &value, &index);
+    if(!parsed)
+        return NULL;
+
+    if (-1 == PyObject_SetItem(object, index, value))
+        return NULL;
+    Py_RETURN_NONE;
+}
+
 static PyMethodDef modapply_methods[] = {
     {
         "apply",
         (PyCFunction) apply,
         METH_VARARGS | METH_KEYWORDS,
         "Pure C implementation of apply, with optional safety checks.",
+    }, {
+        "suply",
+        (PyCFunction) suply,
+        METH_VARARGS | METH_KEYWORDS,
+        "Star-apply without safety checks (use at your own risk).",
+    }, {
+        "tuply",
+        (PyCFunction) tuply,
+        METH_VARARGS | METH_KEYWORDS,
+        "Tuple-apply without safety checks (use at your own risk).",
+    }, {
+        "s_ply",
+        (PyCFunction) s_ply,
+        METH_VARARGS | METH_KEYWORDS,
+        "Star-apply with safety checks.",
+    }, {
+        "t_ply",
+        (PyCFunction) t_ply,
+        METH_VARARGS | METH_KEYWORDS,
+        "Tuple-apply with safety checks.",
+    }, {
+        "getitem",
+        (PyCFunction) getitem,
+        METH_VARARGS | METH_KEYWORDS,
+        "getitem(object, *, index) returns object[index]",
+    }, {
+        "setitem",
+        (PyCFunction) setitem,
+        METH_VARARGS | METH_KEYWORDS,
+        "setitem(object, value, *, index) does object[index] = value",
     }, {
         NULL,
         NULL,
