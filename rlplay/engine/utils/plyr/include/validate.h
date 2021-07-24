b4bf32cfa8ec212442466412de21c249fb38891e
ivannz <ivannz@yandex.ru>
2021-07-24 08:53:55 +0300

    backport const-correct py-is-type from py3.9 into validate.h (bpo-39573)

--- rlplay/engine/utils/plyr/include/validate.h
+++ rlplay/engine/utils/plyr/include/validate.h
@@ -1,5 +1,16 @@
 #include <vector>
 
+// bpo-39573: Make Py_IS_TYPE take const args. Add _PyObject_CAST_CONST. #18799
+#if PY_VERSION_HEX < 0x03090000
+#   define _PyObject_CAST_CONST(op) ((const PyObject*)(op))
+
+    static inline int _Py_IS_TYPE(const PyObject *ob, const PyTypeObject *type) {
+        return ob->ob_type == type;
+    }
+
+#   define Py_IS_TYPE(ob, type) _Py_IS_TYPE(_PyObject_CAST_CONST(ob), type)
+#endif
+
 typedef std::vector<PyObject *> objectstack;
 
 int _validate_dict(PyObject *main, PyObject *rest, objectstack *stack=NULL);
